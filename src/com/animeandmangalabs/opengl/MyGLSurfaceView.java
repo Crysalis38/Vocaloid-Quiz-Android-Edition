4cbf01886d712383887180e49c78b685624d4f2f
Jeroen Mathon <deathslycer@gmail.com>
2014-01-01 23:42:32 +0100

    Added 2 Graphical object, Still in work

--- src/com/animeandmangalabs/opengl/MyGLSurfaceView.java
+++ src/com/animeandmangalabs/opengl/MyGLSurfaceView.java
@@ -1,14 +1,24 @@
 package com.animeandmangalabs.opengl;
 
+import android.content.Context;
 import android.opengl.GLSurfaceView;
 
 /**
  * Created by jeroenmathon on 1/1/14.
  */
 public class MyGLSurfaceView extends GLSurfaceView {
-    public MyGLSurfaceView(OpenGLES20Activity openGLES20Activity) {
+
+
+    public MyGLSurfaceView(Context context) {
         super(context);
 
         setRenderer(new MyRenderer());
+
+        setEGLContextClientVersion(2);
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
     }
+
+
+
+
 }
---
+++ src/com/animeandmangalabs/opengl/Square.java
@@ -0,0 +1,40 @@
+package com.animeandmangalabs.opengl;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+/**
+ * Created by jeroenmathon on 1/1/14.
+ */
+public class Square {
+
+    private FloatBuffer vertexBuffer;
+    private ShortBuffer drawListBuffer;
+
+    static final int COORDS_PER_VERTEX = 3;
+    static float squareCoords[] = {
+           -0.5f,   0.5f, 0.0f,
+           -0.5f,  -0.5f, 0.0f,
+            0.5f,  -0.5f, 0.0f,
+            0.5f,   0.5f, 0.0f };
+
+    private short drawOrder[] = {0, 1, 2, 0, 2, 3};
+
+    public Square(){
+        ByteBuffer bb = ByteBuffer.allocateDirect(
+                squareCoords.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(squareCoords);
+        vertexBuffer.position(0);
+
+        ByteBuffer dlb = ByteBuffer.allocateDirect(
+                drawOrder.length * 2);
+        dlb.order(ByteOrder.nativeOrder());
+        drawListBuffer = dlb.asShortBuffer();
+        drawListBuffer.put(drawOrder);
+        drawListBuffer.position(0);
+    }
+}
