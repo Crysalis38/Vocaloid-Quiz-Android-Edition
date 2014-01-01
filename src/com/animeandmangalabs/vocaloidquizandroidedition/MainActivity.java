1920dcae4231498735d0c3d90ed661350f5e131a
JeroenMathon <deathslycer@gmail.com>
2014-01-01 01:24:26 +0100

    upload late

--- src/com/animeandmangalabs/vocaloidquizandroidedition/MainActivity.java
+++ src/com/animeandmangalabs/vocaloidquizandroidedition/MainActivity.java
@@ -11,8 +11,12 @@ import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import com.animeandmangalabs.quiz_engine.*;
+import android.widget.EditText;
+import android.widget.TextView;
 public class MainActivity extends Activity {
-public static Button btn_start;
+public static Button btn_start,btn_remember_submit;
+public static EditText edit_text_remember;
+public static TextView welcome;
 private AlphaAnimation buttonClick = new AlphaAnimation(10F, 0.5F);
 
 
@@ -22,7 +26,9 @@ private AlphaAnimation buttonClick = new AlphaAnimation(10F, 0.5F);
         setContentView(R.layout.activity_main);
         //onCreate init
         btn_start  = (Button) findViewById(R.id.main_start_button);
-        
+        btn_remember_submit = (Button) findViewById(R.id.btn_submit_remember);
+		welcome = (TextView) findViewById(R.id.EditText01);
+		edit_text_remember = (EditText) findViewById(R.id.edit_text_remember);
         //Btn listener
         btn_start.setOnClickListener(new View.OnClickListener() {
 			
@@ -36,7 +42,20 @@ private AlphaAnimation buttonClick = new AlphaAnimation(10F, 0.5F);
 				
 			}
 		});
-    }
+		btn_remember_submit.setOnClickListener(new View.OnClickListener(){
+
+				@Override
+				public void onClick(View p1)
+				{
+					// TODO: Implement this method
+				String name = quiz_engine.q_data.getString(MainActivity.this,"name","NULL");
+					
+					welcome.setText(name);
+				}
+
+		
+		});
+		}
 
   
    
