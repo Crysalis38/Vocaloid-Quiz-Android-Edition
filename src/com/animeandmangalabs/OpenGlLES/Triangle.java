58a7860fab3d3ecd3736ae5e376fc153c44d364b
Jeroen Mathon <deathslycer@gmail.com>
2014-01-02 16:11:24 +0100

    optimised OpenGLES classes

--- src/com/animeandmangalabs/OpenGlLES/Triangle.java
+++ src/com/animeandmangalabs/OpenGlLES/Triangle.java
@@ -1,5 +1,7 @@
 package com.animeandmangalabs.OpenGlLES;
 
+import android.opengl.GLES20;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
@@ -30,7 +32,16 @@ public class Triangle {
     +       "            * a_Position   \n"     //Multiply the vertex by the matrix to get the final point in
     +       "}                          \n";    //normalized screen coordinates.
 
-    final String fragmentShader
+    final String fragmentShader =
+            "precision mediump float;   \n"     //Set the default precision to medium. We don't need as high of a
+                                                //precision in the fragment shader.
+    +       "varying vec4 V-Color;      \n"     //This is the color from the vertex shader interpolated across the
+                                                //triangle per fragment.
+    +       "void main()                \n"     //The entry point for out fragment shader.
+    +       "{                          \n"
+    +       "   gl_FragColor = v_Color; \n"     //Pass the color directly trough the pipeline.
+    +       "}                          \n";
+
     //How many bytes per float
     private final int mBytesPerFloat = 3;
 
@@ -56,5 +67,66 @@ public class Triangle {
 
         mTriangle1Vertices.put(triangle1VerticesData).position(0);
 
+        //Load in the vertex shader.
+        int vertexShaderHandle = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER);
+
+        if(vertexShaderHandle != 0)
+        {
+            //Pass in the shader source.
+            GLES20.glShaderSource(vertexShaderHandle, vertexShader);
+
+            //Compile the shader.
+            GLES20.glCompileShader(vertexShaderHandle);
+
+            //Get the compilation status.
+            final int[] compileStatus = new int[1];
+            GLES20.glGetShaderiv(vertexShaderHandle, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+
+            //If the compilation failed delete the shader.
+            if (compileStatus[0] == 0)
+            {
+                GLES20.glDeleteShader(vertexShaderHandle);
+                vertexShaderHandle = 0;
+            }
+        }
+
+        if(vertexShaderHandle == 0)
+        {
+            throw new RuntimeException("Error creating vertex shader.");
+        }
+
+        //Create a program object and store the handle to it.
+        int programHandle = GLES20.glCreateProgram();
+
+        if(programHandle != 0)
+        {
+            //Bind the vertex shader to the program.
+            GLES20.glAttachShader(programHandle, vertexShaderHandle);
+
+            //Bind the fragment shader to the program.
+            GLES20.glAttachShader(programHandle, fragmentShaderHandle);
+
+            //Bind attributes.
+            GLES20.glBindAttribLocation(programHandle, 0, "a_Position");
+            GLES20.glBindAttribLocation(programHandle, 1, "a_Color");
+
+            //Link the two shaders together into a program.
+            GLES20.glLinkProgram(programHandle);
+
+            //Get link status.
+            final int[] linkStatus = new int[1];
+            GLES20.glGetProgramiv(programHandle, GLES20.GL_LINK_STATUS, linkStatus, 0);
+
+            //If the link failed, delete the program.
+            if (linkStatus[0] == 0)
+            {
+                GLES20.glDeleteProgram(programHandle);
+                programHandle = 0;
+            }
+        }
+        if (programHandle == 0)
+        {
+            throw new RuntimeException("Error creating program.");
+        }
     }
 }
