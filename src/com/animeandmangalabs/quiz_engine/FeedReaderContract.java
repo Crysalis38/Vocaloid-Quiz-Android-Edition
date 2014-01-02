d442330f012e7bb1ed476b6b1f87167e3078ac59
Jeroen Mathon <deathslycer@gmail.com>
2014-01-03 00:27:59 +0100

    In progress

--- src/com/animeandmangalabs/quiz_engine/FeedReaderContract.java
+++ src/com/animeandmangalabs/quiz_engine/FeedReaderContract.java
@@ -10,7 +10,20 @@ public final class FeedReaderContract {
 
     public static abstract class FeedEntry implements BaseColumns {
         public static final String TABLE_NAME = "entry";
-        public static final String COLUMN_NAME_ENRT_ID = "entryid";
+        public static final String COLUMN_NAME_ENTRY_ID = "entryid";
         public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+        private static final String TEXT_TYPE = " TEXT";
+        private static final String COMMA_SEP = " ,";
+        private static final String SQL_CREATE_ENTRIES =
+        "CREATE_TABLE "  + FeedEntry.TABLE_NAME + " (" +
+                FeedEntry._ID + " INTEGER PRIMARY KEY," +
+                FeedEntry.COLUMN_NAME_ENTRY_ID + TEXT_TYPE + COMMA_SEP +
+                FeedEntry.COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +
+                // Any other options for the CREATE command
+                " )";
+        private  static final String SQL_DELETE_ENTRIES =
+                "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
     }
 }
+
